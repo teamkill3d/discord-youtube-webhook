8bc69ece078bb652dd526663da6d128db04af10f
teamkill3d <90103805+teamkill3d@users.noreply.github.com>
2024-06-23 02:34:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ channel = "put channel here"
 webhook = "put webhook link here"
 
 # variables !!!DO NOT EDIT!!!
-html = requests.get(channel + "/videos").text
-lastUrl = "https://www.youtube.com/watch?v=" + re.search('(?<="videoId":").*?(?=")', html).group()
+lastUrl = "osjidfoascdmrosuierpds"
 
 while True:
     time.sleep(900) # cooldown to not exceed rate limits TODO: this is a stupid solution, will fix when i get better at programming (or if someone fixes it for me)
     
     # look up the channel's videos and pick out the most recent one
+    html = requests.get(channel + "/videos").text
     url = "https://www.youtube.com/watch?v=" + re.search('(?<="videoId":").*?(?=")', html).group()
     
     # anti-spam system, check the latest video's url, if the bot has already sent a notification about it, restart the loop
