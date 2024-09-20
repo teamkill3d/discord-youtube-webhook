9600259c5ff934f62ff0c081a7a2f929cf33c72a
teamkill3d <90103805+teamkill3d@users.noreply.github.com>
2024-09-19 22:00:42 -0400

    made the code more pythonic
    still need to learn how to use google apis

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 # dependencies
-import requests
-import re
-import time
+import requests;
+import re;
+import time;
 
 # config
-channel = "put channel here"
-webhook = "put webhook link here"
+channel = "put channel here";
+webhook = "put webhook link here";
 
-lastUrl = "null"
+lastUrl = "null";
 
 # TODO: actually learn how to use youtube's api and other apis in general
 while True:
-    time.sleep(900) # cooldown to not exceed rate limits
+    time.sleep(900); # cooldown to not exceed rate limits
     
     # look up the channel's videos and pick out the most recent one
-    html = requests.get(channel + "/videos").text
-    url = "https://www.youtube.com/watch?v=" + re.search('(?<="videoId":").*?(?=")', html).group()
+    html = requests.get(channel + "/videos").text;
+    url = "https://www.youtube.com/watch?v=" + re.search('(?<="videoId":").*?(?=")', html).group();
     
     # anti-spam system, check the latest video's url, if the bot has already sent a notification about it, restart the loop
     if url == lastUrl:
-        continue
-    lastUrl = url
+        continue;
+    lastUrl = url;
     
     # format and post the data
     data = {
@@ -28,12 +28,12 @@ while True:
         # "username" : "optional, can be configured directly on discord itself",
         # "avatar_url" : "this is also optional for the same reason"
     }
-    result = requests.post(webhook, json = data)
+    result = requests.post(webhook, json = data);
 
     # check for errors
     try:
-        result.raise_for_status()
+        result.raise_for_status();
     except requests.exceptions.HTTPError as error:
-        print(error)
+        print(error);
     else:
-        print("payload delivered successfully! code {}.".format(result.status_code))
+        print("payload delivered successfully! code {}.".format(result.status_code));
